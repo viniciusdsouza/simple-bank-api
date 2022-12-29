882c7a5b8d7eb67135ca24e41431e259c0e7c808
viniciusdsouza <vduartesantiago@gmail.com>
2022-12-29 17:22:32 -0300

    fix: solve entries list query and remove methods out of scope

--- db/query/entries.sql
+++ db/query/entries.sql
@@ -1,4 +1,4 @@
--- name: CreateEntries :one
+-- name: CreateEntry :one
 INSERT INTO entries (
   account_id,
   amount
@@ -6,22 +6,13 @@ INSERT INTO entries (
   $1, $2
 ) RETURNING *;
 
--- name: GetEntries :one
+-- name: GetEntry :one
 SELECT * FROM entries
 WHERE id = $1 LIMIT 1;
 
 -- name: ListEntries :many
 SELECT * FROM entries
+WHERE account_id = $1
 ORDER BY id
-LIMIT $1
-OFFSET $2;
-
--- name: UpdateEntries :one
-UPDATE entries
-SET amount = $2
-WHERE id = $1
-RETURNING *;
-
--- name: DeleteEntries :exec
-DELETE FROM entries
-WHERE id = $1;
+LIMIT $2
+OFFSET $3;
