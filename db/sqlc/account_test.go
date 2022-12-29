1a50291cf0f96a02a2cdad7066f22d4d7a01c2fc
viniciusdsouza <vduartesantiago@gmail.com>
2022-12-29 19:17:22 -0300

    test: implements update, delete and list Account unit tests

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -7,6 +7,7 @@ package db
 
 import (
 	"context"
+	"database/sql"
 	"testing"
 	"time"
 
@@ -50,4 +51,52 @@ func TestGetAccount(t *testing.T) {
 	require.Equal(t, account1.Balance, account2.Balance)
 	require.Equal(t, account1.Currency, account2.Currency)
 	require.WithinDuration(t, account1.CreatedAt, account2.CreatedAt, time.Second)
+}
+
+func TestUpdateAccount(t *testing.T) {
+	account1 := createRandomAccount(t)
+	cmd := UpdateAccountParams{
+		ID: account1.ID,
+		Balance: util.RandomMoney(0.0, 999999.99),
+	}
+	
+	account2, err := testQueries.UpdateAccount(context.Background(), cmd)
+	require.NoError(t, err)
+	require.NotEmpty(t, account2)
+
+	require.Equal(t, account1.ID, account2.ID)
+	require.Equal(t, account2.Balance, cmd.Balance)
+	require.Equal(t, account1.Owner, account2.Owner)
+	require.Equal(t, account1.Currency, account2.Currency)
+	require.WithinDuration(t, account1.CreatedAt, account2.CreatedAt, time.Second)
+}
+
+func TestDeleteAccount(t *testing.T) {
+	account1 := createRandomAccount(t)
+	err := testQueries.DeleteAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+
+	account2, err := testQueries.GetAccount(context.Background(), account1.ID)
+	require.Error(t, err)
+	require.EqualError(t, err, sql.ErrNoRows.Error())
+	require.Empty(t, account2)
+}
+
+func TestListAccount(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		createRandomAccount(t)
+	}
+
+	cmd := ListAccountsParams{
+		Limit: 5,
+		Offset: 5,
+	}
+
+	accounts, err := testQueries.ListAccounts(context.Background(), cmd)
+	require.NoError(t, err)
+	require.Len(t, accounts, 5)
+
+	for _, account := range accounts {
+		require.NotEmpty(t, account)
+	}
 }
