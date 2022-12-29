6fa04cec5f511b525e3a24fa18a4cda269713e39
viniciusdsouza <vduartesantiago@gmail.com>
2022-12-29 17:17:22 -0300

    feat: use sqlc to generate list, update and delete query methods for Entries

--- db/sqlc/entries.sql.go
+++ db/sqlc/entries.sql.go
@@ -35,6 +35,16 @@ func (q *Queries) CreateEntries(ctx context.Context, arg CreateEntriesParams) (E
 	return i, err
 }
 
+const deleteEntries = `-- name: DeleteEntries :exec
+DELETE FROM entries
+WHERE id = $1
+`
+
+func (q *Queries) DeleteEntries(ctx context.Context, id int64) error {
+	_, err := q.db.ExecContext(ctx, deleteEntries, id)
+	return err
+}
+
 const getEntries = `-- name: GetEntries :one
 SELECT id, account_id, amount, created_at FROM entries
 WHERE id = $1 LIMIT 1
@@ -42,6 +52,70 @@ WHERE id = $1 LIMIT 1
 
 func (q *Queries) GetEntries(ctx context.Context, id int64) (Entry, error) {
 	row := q.db.QueryRowContext(ctx, getEntries, id)
+	var i Entry
+	err := row.Scan(
+		&i.ID,
+		&i.AccountID,
+		&i.Amount,
+		&i.CreatedAt,
+	)
+	return i, err
+}
+
+const listEntries = `-- name: ListEntries :many
+SELECT id, account_id, amount, created_at FROM entries
+ORDER BY id
+LIMIT $1
+OFFSET $2
+`
+
+type ListEntriesParams struct {
+	Limit  int32 `json:"limit"`
+	Offset int32 `json:"offset"`
+}
+
+func (q *Queries) ListEntries(ctx context.Context, arg ListEntriesParams) ([]Entry, error) {
+	rows, err := q.db.QueryContext(ctx, listEntries, arg.Limit, arg.Offset)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var items []Entry
+	for rows.Next() {
+		var i Entry
+		if err := rows.Scan(
+			&i.ID,
+			&i.AccountID,
+			&i.Amount,
+			&i.CreatedAt,
+		); err != nil {
+			return nil, err
+		}
+		items = append(items, i)
+	}
+	if err := rows.Close(); err != nil {
+		return nil, err
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return items, nil
+}
+
+const updateEntries = `-- name: UpdateEntries :one
+UPDATE entries
+SET amount = $2
+WHERE id = $1
+RETURNING id, account_id, amount, created_at
+`
+
+type UpdateEntriesParams struct {
+	ID     int64  `json:"id"`
+	Amount string `json:"amount"`
+}
+
+func (q *Queries) UpdateEntries(ctx context.Context, arg UpdateEntriesParams) (Entry, error) {
+	row := q.db.QueryRowContext(ctx, updateEntries, arg.ID, arg.Amount)
 	var i Entry
 	err := row.Scan(
 		&i.ID,
